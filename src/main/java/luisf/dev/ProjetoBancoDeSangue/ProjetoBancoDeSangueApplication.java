61d9bf576bef3905e1e523530cc2fbf1a5426c10
luisf <luisfelipefv8@hotmail.com>
2025-06-14 09:29:18 -0300

    First commit

---
+++ src/main/java/luisf/dev/ProjetoBancoDeSangue/ProjetoBancoDeSangueApplication.java
@@ -0,0 +1,13 @@
+package luisf.dev.ProjetoBancoDeSangue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoBancoDeSangueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoBancoDeSangueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/luisf/dev/ProjetoBancoDeSangue/ProjetoBancoDeSangueApplicationTests.java
@@ -0,0 +1,13 @@
+package luisf.dev.ProjetoBancoDeSangue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoBancoDeSangueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
