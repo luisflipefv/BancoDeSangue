ddfdde9cb1ed493ac81c1ff8039c3271ac4d4580
luisf <luisfelipefv8@hotmail.com>
2025-07-05 12:39:47 -0300

    fix: adiciona a opção de buscar uma doação pelo seu doador

--- src/main/java/luisf/dev/ProjetoBancoDeSangue/Doacao/DoacaoRepository.java
+++ src/main/java/luisf/dev/ProjetoBancoDeSangue/Doacao/DoacaoRepository.java
@@ -2,5 +2,8 @@ package luisf.dev.ProjetoBancoDeSangue.Doacao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface DoacaoRepository extends JpaRepository<DoacaoModel, Long> {
+    List<DoacaoModel> findByDoadorId(Long doadorId);
 }
