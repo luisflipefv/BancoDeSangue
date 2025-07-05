14071588bcf3a23b5c3adde2060ea5ae6d421847
luisf <luisfelipefv8@hotmail.com>
2025-07-04 23:39:56 -0300

    feat: adiciona a verificação do cpf no doadorRepository

--- src/main/java/luisf/dev/ProjetoBancoDeSangue/Doador/DoadorRepository.java
+++ src/main/java/luisf/dev/ProjetoBancoDeSangue/Doador/DoadorRepository.java
@@ -3,4 +3,5 @@ package luisf.dev.ProjetoBancoDeSangue.Doador;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DoadorRepository extends JpaRepository<DoadorModel, Long> {
+    boolean existsByCpf(String cpf);
 }
